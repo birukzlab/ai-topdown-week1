46ab683f844d17da6008bd5a090f4ccbd9f1519f
birukzlab <birukalemu63@gmail.com>
2025-12-01 13:27:50 -0600

    Add text transform helper function

--- text_utils_app/utils.py
+++ text_utils_app/utils.py
@@ -5,3 +5,13 @@ def get_stats(text: str):
         "words": len(words),
         "reading_time_min": len(words) / 200.0,  # naive 200 wpm
     }
+
+def transform_text(text: str, mode:str):
+    if mode == "UPPERCASE":
+        return text.upper()
+    if mode == "lowercase":
+        return text.lower()
+    if mode == "Title Case":
+        return text.title()
+    return text
+
